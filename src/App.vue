f26a6136fcc120149e03722618d112b915b7bdbc
Michael Bany <mobany526@gmail.com>
2025-05-27 15:35:54 +0200

    založeno na shadcn

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Container from "./components/Container.vue";
+import UiButton from "./components/ui/button/UiButton.vue";
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center p-8 max-w-5xl mx-auto">
+    
+    <Container name="Button">
+      <UiButton variant="destructive">Hello</UiButton>
+    </Container>
+  </div>
+</template>
---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string;
+}>();
+</script>
+<template>
+  <div class="w-full">
+    <span>{{ props.name }}</span>
+    <div
+      class="w-full flex gap-2 items-center justify-center border p-5 rounded-lg"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
