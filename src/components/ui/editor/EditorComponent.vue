f100e0003c7b31c10f708c83bbbad53c7661e9f2
Michael Bany <mobany526@gmail.com>
2025-05-28 16:32:15 +0200

    integrace editoru

--- src/components/ui/editor/EditorComponent.vue
+++ src/components/ui/editor/EditorComponent.vue
@@ -1,13 +1,35 @@
 <template>
-  <EditorContent :editor="editor" />
+  <div class="w-full h-full flex flex-col gap-4">
+    <div class="flex justify-start items-center gap-1">
+      <UiButton @click="editor.chain().focus().toggleBold().run()" :class="{ 'text-red-500': editor?.isActive('bold') }" size="icon" variant="secondary">
+        <UiIcon name="lucide:bold" />
+      </UiButton>
+      <UiButton @click="editor.chain().focus().toggleItalic().run()" :class="{ 'text-red-500': editor?.isActive('italic') }" size="icon" variant="secondary">
+        <UiIcon name="lucide:italic" />
+      </UiButton>
+      <UiButton @click="editor.chain().focus().toggleStrike().run()" :class="{ 'text-red-500': editor?.isActive('strike') }" size="icon" variant="secondary">
+        <UiIcon name="lucide:strikethrough" />
+      </UiButton>
+    </div>
+    <div>
+      <EditorContent :editor="editor" class="w-full h-full" />
+    </div>
+  </div>
 </template>
 
 <script setup>
-  import { useEditor, EditorContent } from '@tiptap/vue-3'
-  import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent } from "@tiptap/vue-3";
+import StarterKit from "@tiptap/starter-kit";
+import UiButton from "../button/UiButton.vue";
+import UiIcon from "../UiIcon.vue";
 
-  const editor = useEditor({
-    content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
-    extensions: [StarterKit],
-  })
+const editor = useEditor({
+  editorProps: {
+    attributes: {
+      class: "prose prose-md focus:outline-none",
+    },
+  },
+  content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
+  extensions: [StarterKit],
+});
 </script>
