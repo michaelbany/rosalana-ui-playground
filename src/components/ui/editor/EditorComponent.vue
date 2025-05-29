0127e898bd70737fcce1d778e20c8ee968263454
Michael Bany <mobany526@gmail.com>
2025-05-29 13:01:29 +0200

    s

--- src/components/ui/editor/EditorComponent.vue
+++ src/components/ui/editor/EditorComponent.vue
@@ -1,19 +1,55 @@
 <template>
   <div class="w-full h-full flex flex-col gap-4">
-    <div class="flex justify-start items-center gap-1">
-      <UiButton @click="editor.chain().focus().toggleBold().run()" :class="{ 'text-red-500': editor?.isActive('bold') }" size="icon" variant="secondary">
-        <UiIcon name="lucide:bold" />
-      </UiButton>
-      <UiButton @click="editor.chain().focus().toggleItalic().run()" :class="{ 'text-red-500': editor?.isActive('italic') }" size="icon" variant="secondary">
-        <UiIcon name="lucide:italic" />
-      </UiButton>
-      <UiButton @click="editor.chain().focus().toggleStrike().run()" :class="{ 'text-red-500': editor?.isActive('strike') }" size="icon" variant="secondary">
-        <UiIcon name="lucide:strikethrough" />
-      </UiButton>
+    <div class="flex items-center gap-4">
+      <div class="flex justify-start items-center gap-1">
+        <UiButton
+          @click="editor.chain().focus().toggleBold().run()"
+          :class="{ 'text-red-500': editor?.isActive('bold') }"
+          size="icon"
+          variant="secondary"
+        >
+          <UiIcon name="lucide:bold" />
+        </UiButton>
+        <UiButton
+          @click="editor.chain().focus().toggleItalic().run()"
+          :class="{ 'text-red-500': editor?.isActive('italic') }"
+          size="icon"
+          variant="secondary"
+        >
+          <UiIcon name="lucide:italic" />
+        </UiButton>
+        <UiButton
+          @click="editor.chain().focus().toggleStrike().run()"
+          :class="{ 'text-red-500': editor?.isActive('strike') }"
+          size="icon"
+          variant="secondary"
+        >
+          <UiIcon name="lucide:strikethrough" />
+        </UiButton>
+      </div>
+      <div class="flex justify-start items-center gap-1">
+        <UiButton
+          @click="editor.chain().focus().undo().run()"
+          size="icon"
+          variant="secondary"
+        >
+          <UiIcon name="lucide:undo" />
+        </UiButton>
+        <UiButton
+          @click="editor.chain().focus().redo().run()"
+          size="icon"
+          variant="secondary"
+        >
+          <UiIcon name="lucide:redo" />
+        </UiButton>
+      </div>
     </div>
     <div>
       <EditorContent :editor="editor" class="w-full h-full" />
     </div>
+    <pre>
+      {{ editor?.getJSON() }}
+    </pre>
   </div>
 </template>
 
@@ -22,6 +58,7 @@ import { useEditor, EditorContent } from "@tiptap/vue-3";
 import StarterKit from "@tiptap/starter-kit";
 import UiButton from "../button/UiButton.vue";
 import UiIcon from "../UiIcon.vue";
+import { onBeforeMount, ref } from "vue";
 
 const editor = useEditor({
   editorProps: {
